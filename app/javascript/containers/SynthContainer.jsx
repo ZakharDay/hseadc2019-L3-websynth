ad5e21dc4dd6978601c6d2565f0a736476d1df0b
ZakharDay <zakhar.day@gmail.com>
2021-09-25 18:45:13 +0300

    Work snapshot

--- app/javascript/containers/SynthContainer.jsx
+++ app/javascript/containers/SynthContainer.jsx
@@ -1,9 +1,35 @@
 import React, { PureComponent } from 'react'
+import * as Tone from 'tone'
+
+import Button from '../components/Button'
 import Oscillator from '../components/Oscillator'
 
 export default class SynthContainer extends PureComponent {
   constructor(props) {
     super(props)
+
+    this.state = {
+      audioContextStarted: false
+    }
+  }
+
+  playSound = () => {
+    const synth = new Tone.Synth().toDestination()
+    const now = Tone.now()
+    // trigger the attack immediately
+    synth.triggerAttack('C4', now)
+    // wait one second before triggering the release
+    synth.triggerRelease(now + 1)
+  }
+
+  audioContextStart = () => {
+    this.setState({
+      audioContextStarted: true
+    })
+  }
+
+  renderAudioContextStartButton = () => {
+    return <Button text="START AUDIO" handleClick={this.audioContextStart} />
   }
 
   renderOscillators = () => {
@@ -29,6 +55,16 @@ export default class SynthContainer extends PureComponent {
   }
 
   render() {
-    return <div>{this.renderOscillators()}</div>
+    const { audioContextStarted } = this.state
+
+    return (
+      <div>
+        {audioContextStarted === true
+          ? this.renderOscillators()
+          : this.renderAudioContextStartButton()}
+
+        <Button text="PLAY SOUND" handleClick={this.playSound} />
+      </div>
+    )
   }
 }
